9583154690319b027af8bc9cbfe05c173953a6f5
danres2025 <danres2025@gmail.com>
2026-03-24 19:05:57 -0700

    New function added

--- src/mat_operations.py
+++ src/mat_operations.py
@@ -2,4 +2,9 @@ def add(a,b):
     return a+b
 
 def sub(a,b):
-    return a-b 
+    return a-b
+
+def div(a,b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b 
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,4 @@
-from src.mat_operations import add,sub
+from src.mat_operations import add,sub,div
 
 def test_add():
     assert add(2,3)==5
@@ -9,3 +9,8 @@ def test_sub():
     assert sub(4,3)==1
     assert sub(3,3)==0
     assert sub(2,3)==-1
+
+def test_div():
+    assert div(6,3)==2
+    assert div(10,2)==5
+    assert div(7,2)==3.5
